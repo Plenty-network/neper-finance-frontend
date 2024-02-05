f7deca43b0575501758222f2980b8c546485138e
ChainEx <57598532+AniketSindhu@users.noreply.github.com>
2024-02-05 10:16:55 +0530

    fix: added os

--- config-overrides.js
+++ config-overrides.js
@@ -8,7 +8,10 @@ module.exports = function override(config, env) {
     http: require.resolve("stream-http"),
     zlib: require.resolve("browserify-zlib"),
     path: require.resolve("path-browserify"),
-    crypto: require.resolve("crypto-browserify")
+    crypto: require.resolve("crypto-browserify"),
+    os: require.resolve("os-browserify"),
+    assert: require.resolve("assert"),
+    url: require.resolve("url")
   };
 
   return config;
